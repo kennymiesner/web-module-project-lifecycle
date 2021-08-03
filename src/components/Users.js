8b77a516b892cc05d040e6eccb174ba72ab64a31
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-03 17:02:04 -0500

    Removed stray console.logs

--- src/components/Users.js
+++ src/components/Users.js
@@ -3,11 +3,9 @@ import User from './User'
 
 class Users extends React.Component {
   componentDidMount() {
-    console.log("Pokemon: App Mounts")
   }
   
   render() {
-    console.log("Pokemon: App renders")
     return (
       <>
         {this.props.users.map(user => (
